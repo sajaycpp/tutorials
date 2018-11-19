1d9bd1551f7173487493d3170cf3626b15ecb810
sajaycpp <sajaycp@gmail.com>
2018-11-20 00:49:45 +0530

    Setting up a React project from scratch

--- react/reactProject/webpack/webpack.dev.config.js
+++ react/reactProject/webpack/webpack.dev.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
+var parentDir = path.join(__dirname, '../');
+
 module.exports = {
 	entry: [
 		path.join(__dirname, '../index.js')
@@ -8,12 +10,20 @@ module.exports = {
 	module: {
 		loaders: [{
 			test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},{
-				test: /\.less$/,
-				loaders: ["style-loader", "css-loader", "less-loader"]
-			}
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}, {
+			test: /\.less$/,
+			loaders: ["style-loader", "css-loader", "less-loader"]
+		}
 		]
 	},
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
 }
